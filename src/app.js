43b7895cbc84059537f6d852f5a06496ecf252e4
bakram4 <bakram4@myseneca.ca>
2023-09-15 14:44:20 -0400

    initial fragments API endpoints&JWT authentication

--- src/app.js
+++ src/app.js
@@ -19,8 +19,16 @@ const pino = require('pino-http')({
   logger,
 });
 
+// import passport and our authentication strategy
+const passport = require('passport');
+const authenticate = require('./auth');
+
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
+
+// Set up our passport authentication middleware
+passport.use(authenticate.strategy());
+app.use(passport.initialize());
 
 // use pino logging middleware
 app.use(pino);
